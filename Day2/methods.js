9009a852fc88f26ec53c031a95349698e6c35c0a
shaikhshoieb <52440672+shaikhshoieb@users.noreply.github.com>
2020-07-16 02:06:22 +0530

    Add files via upload

---
+++ Day2/methods.js
@@ -0,0 +1,44 @@
+let text = "The quick brown fox jumps over the wall";
+console.log(text)
+console.log(text.repeat(4))
+console.log(text.replace("fox","dog"))
+console.log(text.search("e"))
+console.log(text.anchor("hey"))
+console.log(text.bold());
+console.log(text.codePointAt(20))
+console.log(text.fontcolor("red"))
+console.log(text.fontsize(20))
+console.log(text.includes("shoieb",10))
+console.log(text.italics())
+console.log(text.slice(10,20))
+console.log(text.small())
+console.log(text.split("o",2))
+console.log(text.startsWith("e",2))
+console.log(text.substring(2,19))
+console.log(text.sup())
+console.log(text.toLocaleLowerCase())
+console.log(text.toLocaleUpperCase())
+console.log(text.trim())
+console.log(text.valueOf())
+
+
+
+
+let arr = ['one', true, 32, ['1','2']];
+let arr2 = ['rio', 'berlin','alicia'];
+console.log(arr.copyWithin(arr2))
+console.log(arr.entries())
+console.log(arr.fill('one',0,1))
+console.log(arr.indexOf(true))
+console.log(arr.join(arr2))
+console.log(arr.keys())
+console.log(arr.lastIndexOf('1'))
+console.log(arr.length)
+console.log(arr.pop())
+console.log(arr.push(["1", "2"]))
+console.log(arr.reverse())
+console.log(arr.shift())
+console.log(arr.slice(2))
+console.log(arr.splice(1))
+console.log(arr.toLocaleString())
+console.log(arr.values)
